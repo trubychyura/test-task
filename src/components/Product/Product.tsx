969ff6563ca77d54eceb2534a936b1b623a1551d
Yura <trubychyura@gmail.com>
2021-03-23 16:20:46 +0200

    project has been fixed  acording to comments

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -5,11 +5,11 @@ import { ProductProps } from '../../types';
 import { StyledCol, StyledImage, StyledRow } from '../../styled/StyledProduct';
 
 const Product: FC<ProductProps> = ({ id, name, img }) => (
-  <StyledRow className='product'>
-    <StyledCol>
+  <StyledRow>
+    <StyledCol xs={{ span: 12, order: 'last' }} md={{ order: 'first' }}>
       <Link to={`/${id}`}>{name}</Link>
     </StyledCol>
-    <StyledCol>
+    <StyledCol xs={{ span: 12, order: 'first' }} md={{ order: 'last' }}>
       <StyledImage src={img} />
     </StyledCol>
   </StyledRow>
